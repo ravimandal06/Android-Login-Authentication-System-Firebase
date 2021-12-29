34c935018a4b47a79184f61b1ffbf035d726ad59
ravimandal06 <ravimandal449@gmail.com>
2021-12-30 01:50:35 +0530

    Android Login Authentication

---
+++ app/src/main/java/com/example/android/loginframe/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.android.loginframe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEmail , mPass;
+    private TextView mTextView;
+    private Button LoginBtn;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        mEmail = findViewById(R.id.email_login);
+        mPass = findViewById(R.id.pass_login);
+        LoginBtn = findViewById(R.id.login_btn);
+        mTextView = findViewById(R.id.textView2);
+
+        mAuth = FirebaseAuth.getInstance();
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this , MainActivity.class));
+            }
+        });
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+    }
+    private void loginUser(){
+        String email = mEmail.getText().toString();
+        String pass = mPass.getText().toString();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!pass.isEmpty()){
+                mAuth.signInWithEmailAndPassword(email , pass)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this , HomeActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Login Failed !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                mPass.setError("Empty Fields Are not Allowed");
+            }
+        }else if(email.isEmpty()){
+            mEmail.setError("Empty Fields Are not Allowed");
+        }else{
+            mEmail.setError("Pleas Enter Correct Email");
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/android/loginframe/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.android.loginframe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmail , mPass;
+    private TextView mTextView;
+    private Button signUpBtn;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        mEmail = findViewById(R.id.email_reg);
+        mPass = findViewById(R.id.pass_reg);
+        mTextView = findViewById(R.id.textView);
+        signUpBtn = findViewById(R.id.registration_btn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+            }
+        });
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
+    }
+    private void createUser(){
+        String email = mEmail.getText().toString();
+        String pass = mPass.getText().toString();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!pass.isEmpty()){
+                mAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(MainActivity.this, "Registered Successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Registration Error !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                mPass.setError("Empty Fields Are not Allowed");
+            }
+        }else if(email.isEmpty()){
+            mEmail.setError("Empty Fields Are not Allowed");
+        }else{
+            mEmail.setError("Pleas Enter Correct Email");
+        }
+
+    }
+}
